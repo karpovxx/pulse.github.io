1097cd8fd2109a268ee98cc6ebc3eb8a662faa70
Ruslan Karpov <ruslankarpov64@gmail.com>
2024-04-03 23:34:45 +0500

    sixth

--- src/js/script.js
+++ src/js/script.js
@@ -47,6 +47,27 @@ $(document).ready(function(){
 	};
 	toggleClass('.catalog-item__link');
 	toggleClass('.catalog-item__back');
-   
+
+ // modal 
+ 
+$('[data-modal=consultation]').on('click', function() {
+	$('.overlay, #consultation').fadeIn('slow')
+})  
 });
+$('.modal__close').on('click', function(){
+	$('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+});
+$('.button_mini').on('click', function(){
+	$('.overlay, #order').fadeIn('slow')
+});
+$('.button_mini').each(function(i){
+	$(this).on('click', function(){
+		$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+		$('.overlay, #order').fadeIn('slow')
+	})
+});
+
+ 
+
+
 		
