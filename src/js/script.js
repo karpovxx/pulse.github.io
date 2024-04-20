366815844953c217d04bc4edbf280cb433d4a13d
Ruslan Karpov <ruslankarpov64@gmail.com>
2024-04-20 23:10:48 +0500

    tenth

--- src/js/script.js
+++ src/js/script.js
@@ -203,11 +203,13 @@ $(document).ready(function(){
 			$('.pageup').fadeOut();
 		}
 	});
-	$("a[href^='#']").click(function(){
+	$("a[href^='#up']").click(function(){
 		var _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
-	})
+	});
+
+	new WOW().init()
 });
 
  
